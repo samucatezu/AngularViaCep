bb72557628d4d3f84f5c31401937a1f7f5d71416
Samuel Almeida <dev3.gbmlogistica@outlook.com>
2021-12-22 10:12:46 -0300

    Design update

--- angularViaCep/src/app/app.module.ts
+++ angularViaCep/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 import { MatIconModule } from '@angular/material/icon';
 
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -26,8 +28,10 @@ import { RouterModule } from '@angular/router';
   imports: [
     BrowserModule,
     MatToolbarModule,
+    MatButtonModule,
     MatCardModule,
     BrowserAnimationsModule,
+
     MatFormFieldModule,
     MatIconModule,
     ReactiveFormsModule,
